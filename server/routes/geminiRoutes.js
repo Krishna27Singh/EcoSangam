2234b65039274168fe07a2a7d1e021036623f8bf
Krishna27Singh <krishnasingh97857@gmail.com>
2025-07-12 19:46:20 +0530

    backend

--- server/routes/geminiRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { undoImpact } = require('../controllers/geminiController');
-
-router.post('/', undoImpact);
-
-module.exports = router;
--- server/routes/tipsRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getTip } = require("../controllers/tipsController");
-
-router.post("/", getTip);
-
-module.exports = router;
